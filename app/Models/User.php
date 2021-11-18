79d12ebebf562829436f0b42086af5527be38c3d
greylight2 <ya-vam-ne@mail.ru>
2021-11-18 16:32:45 +0300

    настроено меню пользователя

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,18 +42,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    // public function adminlte_image()
-    // {
-    //     return 'https://picsum.photos/300/300';
-    // }
+    public function adminlte_image()
+    {
+        return 'https://picsum.photos/300/300';
+    }
 
-    // public function adminlte_desc()
-    // {
-    //     return 'That\'s a nice guy';
-    // }
+    public function adminlte_desc()
+    {
+        return 'классный парень';
+    }
 
-    // public function adminlte_profile_url()
-    // {
-    //     return 'profile/username';
-    // }
+    public function adminlte_profile_url()
+    {
+        return 'profile/username';
+    }
 }
