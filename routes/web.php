e0d10c2332faabb39d8159e0e4d4d13f3f1c2814
greylight2 <ya-vam-ne@mail.ru>
2021-11-18 16:13:46 +0300

    Установлены файлы для аутентификации

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', [AdminController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
